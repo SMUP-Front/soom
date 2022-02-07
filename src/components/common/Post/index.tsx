38593af9b8e7d62648ec69b7c7e913627fa8905e
eastcopper <ldh7228@gmail.com>
2022-02-08 02:31:59 +0900

    ⚡ :: (#12)[Post] Add Explain

--- src/components/common/Post/index.tsx
+++ src/components/common/Post/index.tsx
@@ -17,6 +17,11 @@ export default function Post() {
             </span>
             <img src={menu} alt="" />
           </S.Top>
+          <S.Explain>
+            이번주 수요일 영어 단어 수행평가 있습니다. 모두 7-1 학습지 단어
+            외우고 시험 보시기 바랍니다. 이번주 수요일 영어 단어 수행평가
+            있습니다. 모두 7-1 학습지 단어 외우고 시험 보시기 바랍니다. 
+          </S.Explain>
         </main>
       </S.Post>
     </>
