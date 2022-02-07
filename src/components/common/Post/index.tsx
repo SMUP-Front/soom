5e5ebc6b1e4673a6129a36d9e84bcfe41313f56c
eastcopper <ldh7228@gmail.com>
2022-02-08 02:14:44 +0900

    📑 :: (#5)[Add Post]

---
+++ src/components/common/Post/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import * as S from "./styles";
+import menu from "../../../asset/img/menu.png";
+
+export default function Post() {
+  return (
+    <>
+    </>
+  );
+}
