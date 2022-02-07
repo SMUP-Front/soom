652e8429ab4b9ea4513c9d10b1007f149a9694ec
eastcopper <ldh7228@gmail.com>
2022-02-08 02:16:21 +0900

    ⚡ :: (#6)[Add Post]

--- src/components/common/Post/index.tsx
+++ src/components/common/Post/index.tsx
@@ -5,6 +5,20 @@ import menu from "../../../asset/img/menu.png";
 export default function Post() {
   return (
     <>
+      <S.Post>
+        <main>
+          <S.Top>
+            <span>
+              <img src="" alt="" />
+              <div>
+                <S.Class>1-3반</S.Class>
+                <S.Time>30분전</S.Time>
+              </div>
+            </span>
+            <img src={menu} alt="" />
+          </S.Top>
+        </main>
+      </S.Post>
     </>
   );
 }
