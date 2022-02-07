da11d0aba4a22342a78625da96b81f48c5b18d15
eastcopper <ldh7228@gmail.com>
2022-02-08 01:40:12 +0900

    📑 :: (#2)[Add Group]

---
+++ src/components/common/Group/index.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import left from "../../../asset/img/left-arrow-angle.png";
+import right from "../../../asset/img/right-arrow-angle.png";
+import * as S from "./styles";
+
+export default function Group() {
+  return (
+    <>
+      <S.Group>
+        <main>
+          <img src={left} alt="" />
+          <div>
+            <div>
+              <img src="" alt="그룹 프로필 사진" />
+            </div>
+            <div>
+              <img src="" alt="그룹 프로필 사진" />
+            </div>
+            <div>
+              <img src="" alt="그룹 프로필 사진" />
+            </div>
+            <div>
+              <img src="" alt="그룹 프로필 사진" />
+            </div>
+            <div>
+              <img src="" alt="그룹 프로필 사진" />
+            </div>
+          </div>
+          <img src={right} alt="" />
+        </main>
+      </S.Group>
+    </>
+  );
+}
