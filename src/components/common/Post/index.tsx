bb98df03fe9d1e906eef3b95b04b71f99fcc378e
eastcopper <ldh7228@gmail.com>
2022-02-08 15:30:10 +0900

    ⚡ :: (#14)[Post] Add Thumbnail

--- src/components/common/Post/index.tsx
+++ src/components/common/Post/index.tsx
@@ -20,8 +20,26 @@ export default function Post() {
           <S.Explain>
             이번주 수요일 영어 단어 수행평가 있습니다. 모두 7-1 학습지 단어
             외우고 시험 보시기 바랍니다. 이번주 수요일 영어 단어 수행평가
-            있습니다. 모두 7-1 학습지 단어 외우고 시험 보시기 바랍니다. 
+            있습니다. 모두 7-1 학습지 단어 외우고 시험 보시기 바랍니다.
           </S.Explain>
+          <S.Thumbnail>
+            <S.BigImage>
+              <img src="" alt="" />
+            </S.BigImage>
+            <S.BigImage>
+              <img src="" alt="" />
+            </S.BigImage>
+            <S.SmallImage>
+              <img src="" alt="" />
+            </S.SmallImage>
+            <S.SmallImage>
+              <img src="" alt="" />
+            </S.SmallImage>
+            <S.SmallImage>
+              <img src="" alt="" />
+            </S.SmallImage>
+            {/* 나중에 map함수 돌리기 big - 2, small - 3 */}
+          </S.Thumbnail>
         </main>
       </S.Post>
     </>
