d5339a97e0645f4f6fe615cbfcf95e67fdcb59b6
eastcopper <ldh7228@gmail.com>
2022-02-10 00:13:38 +0900

    ⚡ :: (#36)[SoomGroup] Design Club

--- src/components/page/soomGroup/styles.js
+++ src/components/page/soomGroup/styles.js
@@ -25,4 +25,29 @@ export const Top = styled.div`
 export const Club = styled.div`
   display: flex;
   justify-content: center;
+
+  main {
+    display: grid;
+    grid-template-columns: repeat(5, 160px);
+    grid-row-gap: 43px;
+    grid-column-gap: 94px;
+  }
+`;
+
+export const PageNation = styled.div`
+  margin-top: 111px;
+  display: flex;
+  justify-content: center;
+  margin-bottom: 99px;
+  div {
+    width: 54px;
+    display: flex;
+    justify-content: space-between;
+  }
+`;
+
+export const AddButton = styled.div`
+  position: absolute;
+  right: 142px;
+  top: 908px;
 `;
