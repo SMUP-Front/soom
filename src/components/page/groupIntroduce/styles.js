c1e855aba3a6db4a9c13ac15843abe8d137599dc
eastcopper <ldh7228@gmail.com>
2022-02-10 01:11:01 +0900

    ⚡ :: (#39)[GroupIntroduce] Design banner

---
+++ src/components/page/groupIntroduce/styles.js
@@ -0,0 +1,40 @@
+import styled from "styled-components";
+
+export const Banner = styled.div`
+  width: 1920px;
+  height: 230px;
+  background: #c4c4c4;
+`;
+
+export const Top = styled.div`
+  margin-left: 494px;
+  width: 1083px;
+  display: flex;
+  justify-content: space-between;
+`;
+
+export const Request = styled.button``;
+
+export const Title = styled.div`
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  margin-top: 15px;
+
+  hr {
+    width: 173px;
+    height: 1px;
+    border: none;
+    background-color: #242424;
+  }
+`;
+
+export const Profile = styled.span`
+  font-size: 24px;
+  font-weight: bold;
+`;
+
+export const Tag = styled.span`
+  font-size: 14px;
+  color: #A3A3A3;
+`;
