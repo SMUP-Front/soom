261532622bff92e48563e5873c42c4c1a81413c6
eastcopper <ldh7228@gmail.com>
2022-02-09 22:36:38 +0900

    ⚡ :: (#28)[SoomGroup] Design Top

---
+++ src/components/page/soomGroup/styles.js
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const Group = styled.div``;
+
+export const Top = styled.div`
+  margin-top: 36px;
+  margin-bottom: 49px;
+  display: flex;
+  justify-content: center;
+  main {
+    width: 595px;
+    display: flex;
+    justify-content: space-between;
+  }
+`;
