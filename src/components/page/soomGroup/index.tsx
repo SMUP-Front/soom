8404a97c848736e5b265c8c8123d13b9d83110ba
eastcopper <ldh7228@gmail.com>
2022-02-09 22:36:26 +0900

    ⚡ :: (#27)[SoomGroup] Add Top

---
+++ src/components/page/soomGroup/index.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import * as S from "./styles";
+import web from "../../../asset/img/webGroup.png";
+import app from "../../../asset/img/appGroup.png";
+import backend from "../../../asset/img/backendGroup.png";
+import etc from "../../../asset/img/etcGroup.png";
+
+export default function SoomGroup() {
+  return (
+    <>
+      <S.Group>
+        <S.Top>
+          <main>
+            <img src={web} alt="" />
+            <img src={app} alt="" />
+            <img src={backend} alt="" />
+            <img src={etc} alt="" />
+          </main>
+        </S.Top>
+      </S.Group>
+    </>
+  );
+}
