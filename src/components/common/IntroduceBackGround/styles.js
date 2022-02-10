66857f089f2126eb4e6a36a15958705fab2d0545
eastcopper <ldh7228@gmail.com>
2022-02-10 17:54:43 +0900

    ⚡ :: (#48)[IntroduceBackGround] Design BackGround

---
+++ src/components/common/IntroduceBackGround/styles.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const BackGround = styled.div`
+  display: flex;
+  justify-content: center;
+  margin-top: 7%;
+  img {
+    object-fit: cover;
+  }
+`;
