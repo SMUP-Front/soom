4b2a54d397bd681e462f2a55e2a5df9d9f77fd74
eastcopper <ldh7228@gmail.com>
2022-02-11 23:34:18 +0900

    ⚡ :: (#53)[CreateGroup] Add word

--- src/components/page/createGroup/index.tsx
+++ src/components/page/createGroup/index.tsx
@@ -10,33 +10,43 @@ export default function CreateGroup() {
     <>
       <S.Title>SMUP</S.Title>
       <S.Main>
-        <S.ProfileImage>
-          <img src={uploadImage1} alt="" />
-        </S.ProfileImage>
-        <S.BannerImage>
-          <img src={uploadImage2} alt="" />
-        </S.BannerImage>
-        <S.GroupProperty>
-          <S.Property>
-            {property.map((item) => (
-              <button>{item}</button>
-            ))}
-          </S.Property>
-          <S.Branch>
-            {branch.map((item) => (
-              <button>{item}</button>
-            ))}
-          </S.Branch>
-        </S.GroupProperty>
-        <S.GroupName>
-          <input placeholder="비속어, 은어는 사용하지 말아주세요." />
-        </S.GroupName>
-        <S.GroupInformation>
-          <textarea placeholder="100자 이내로 작성해 주세요." />
-        </S.GroupInformation>
-        <S.Create>
-          <button>그룹 만들기</button>
-        </S.Create>
+        <S.Word>
+          <S.WordProfile>프로필 사진</S.WordProfile>
+          <S.WordBanner>배너 이미지</S.WordBanner>
+          <S.WordProperty>그룹 속성</S.WordProperty>
+          <S.WordName>그룹 이름</S.WordName>
+          <S.WordExplain>그룹 설명</S.WordExplain>
+        </S.Word>
+        <hr />
+        <S.Information>
+          <S.ProfileImage>
+            <img src={uploadImage1} alt="" />
+          </S.ProfileImage>
+          <S.BannerImage>
+            <img src={uploadImage2} alt="" />
+          </S.BannerImage>
+          <S.GroupProperty>
+            <S.Property>
+              {property.map((item) => (
+                <button>{item}</button>
+              ))}
+            </S.Property>
+            <S.Branch>
+              {branch.map((item) => (
+                <button>{item}</button>
+              ))}
+            </S.Branch>
+          </S.GroupProperty>
+          <S.GroupName>
+            <input placeholder="비속어, 은어는 사용하지 말아주세요." />
+          </S.GroupName>
+          <S.GroupInformation>
+            <textarea placeholder="100자 이내로 작성해 주세요." />
+          </S.GroupInformation>
+          <S.Create>
+            <button>그룹 만들기</button>
+          </S.Create>
+        </S.Information>
       </S.Main>
     </>
   );
