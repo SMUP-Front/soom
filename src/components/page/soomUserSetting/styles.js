4f7fe830f9cfe97cfdf721e9079f46939a5ca04a
eastcopper <ldh7228@gmail.com>
2022-02-12 00:30:44 +0900

    ⚡ :: (#58)[SoomUserSetting] Design SoomUserSetting

---
+++ src/components/page/soomUserSetting/styles.js
@@ -0,0 +1,90 @@
+import styled from "styled-components";
+
+export const Main = styled.div`
+  width: 100%;
+  button {
+    width: 84px;
+    height: 27px;
+    border-radius: 12px;
+    border: none;
+    background: #9052df;
+    color: #ffffff;
+    font-size: 18px;
+  }
+`;
+
+export const ProfileImage = styled.div`
+  width: 185px;
+  height: 185px;
+  background-color: #e7e7e7;
+  border-radius: 100px;
+`;
+
+export const ProfileChangeButton = styled.div`
+  display: flex;
+  align-items: center;
+  margin-left: 24px;
+`;
+
+export const Profile = styled.div`
+  display: flex;
+  justify-content: center;
+  margin-top: 320px;
+  margin-left: 96px;
+`;
+
+export const UserInformation = styled.div`
+  margin-left: 84px;
+  display: flex;
+  justify-content: center;
+  margin-top: 19px;
+`;
+
+export const UserName = styled.div`
+  display: flex;
+  justify-content: center;
+  font-size: 24px;
+  color: #242424;
+`;
+
+export const Information = styled.div`
+  width: 209px;
+`;
+
+export const School = styled.div`
+  font-size: 16px;
+  color: #9d9d9d;
+  display: flex;
+  justify-content: center;
+`;
+
+export const InformationChangeButton = styled.div`
+  margin-left: 12px;
+`;
+
+export const Complete = styled.div`
+  display: flex;
+  justify-content: center;
+  margin-top: 157px;
+  margin-bottom: 192px;
+  button {
+    width: 523px;
+    height: 67px;
+    border-radius: 6px;
+    color: white;
+    font-weight: bold;
+    font-size: 24px;
+  }
+`;
+
+export const BackGround1 = styled.div`
+  position: absolute;
+  top: 797px;
+  left: 301px;
+`;
+
+export const BackGround2 = styled.div`
+  position: absolute;
+  top: 757px;
+  right: 256px;
+`;
