c289569ac5144021c1f194c1b52e45ddf4a9cc20
eastcopper <ldh7228@gmail.com>
2022-02-15 01:29:01 +0900

    ⚡ :: (#66)[SoomPost] Add AddButton

--- src/components/page/soomPost/index.tsx
+++ src/components/page/soomPost/index.tsx
@@ -3,13 +3,16 @@ import Group from "../../common/GroupProfile";
 import Post from "../../common/Post";
 import PostChat from "../../common/PostChat";
 import * as S from "./styles";
+import addButton from "../../../asset/img/addButton.png";
 
 export default function SoomPost() {
-
   return (
     <>
+      <S.AddButton>
+        <img src={addButton} alt="" />
+      </S.AddButton>
       <Group />
-      <PostChat />
+      <PostChat/>
       <Post />
       <Post />
       <Post />
