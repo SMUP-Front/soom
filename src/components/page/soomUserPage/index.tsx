ad5b2544449fa7a93482c479ba064bf3d5380902
eastcopper <ldh7228@gmail.com>
2022-02-15 02:10:53 +0900

    ⚡ :: (#67)[SoomUserPage] Add Top

--- src/components/page/soomUserPage/index.tsx
+++ src/components/page/soomUserPage/index.tsx
@@ -1,6 +1,20 @@
+import * as S from "./styles";
+
 export default function SoomUserPage() {
-    return (
-        <>
-        </>
-    )
+  return (
+    <>
+      <S.Main>
+        <S.Profile>
+          <img src="" alt="" />
+        </S.Profile>
+        <S.Name>안진형</S.Name>
+        <hr />
+        <S.School>#광주소프트웨어마이스터고등학교</S.School>
+        <S.ButtonDiv>
+          <S.Button>로그아웃</S.Button>
+          <S.Button>설정</S.Button>
+        </S.ButtonDiv>
+      </S.Main>
+    </>
+  );
 }
