400a85620f3c9a29048b4114714599e1872fe10d
eastcopper <ldh7228@gmail.com>
2022-02-16 19:03:11 +0900

    ⚡ :: (#69)[SoomUserPage] Add Content

--- src/components/page/soomUserPage/index.tsx
+++ src/components/page/soomUserPage/index.tsx
@@ -1,6 +1,16 @@
 import * as S from "./styles";
+import leftArrow from "../../../asset/img/left-arrow-angle.png";
+import rightArrow from "../../../asset/img/right-arrow-angle.png";
+import background3 from "../../../asset/img/backGround3.png";
+import background4 from "../../../asset/img/backGround4.png";
 
 export default function SoomUserPage() {
+  const title: string[] = [
+    "내가 속한 그룹",
+    "대기중인 가입 요청",
+    "대기중인 채팅 요청",
+  ];
+
   return (
     <>
       <S.Main>
@@ -14,6 +24,46 @@ export default function SoomUserPage() {
           <S.Button>로그아웃</S.Button>
           <S.Button>설정</S.Button>
         </S.ButtonDiv>
+
+        {title.map((item) => (
+          <S.Contents>
+            <S.Title>{item}</S.Title>
+            <S.Content>
+              <div>
+                <img src={leftArrow} alt="" />
+              </div>
+              <S.Group>
+                <img src="" alt="" />
+                <img src="" alt="" />
+                <img src="" alt="" />
+                <img src="" alt="" />
+                <img src="" alt="" />
+                <img src="" alt="" />
+                {/* map 함수 돌리기 */}
+              </S.Group>
+              <div>
+                <img src={rightArrow} alt="" />
+              </div>
+            </S.Content>
+            <S.Moment>
+              <span>
+                <div>the moment</div>
+                <div>the moment</div>
+                <div>the moment</div>
+                <div>the moment</div>
+                <div>the moment</div>
+                <div>the moment</div>
+                {/* map 함수 돌리기 */}
+              </span>
+            </S.Moment>
+          </S.Contents>
+        ))}
+        <S.Background3>
+          <img src={background3} alt="" />
+        </S.Background3>
+        <S.Background4>
+          <img src={background4} alt="" />
+        </S.Background4>
       </S.Main>
     </>
   );
