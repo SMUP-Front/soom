d20b4f49a9bb03f13e3cd55c63024b1f19817363
eastcopper <ldh7228@gmail.com>
2022-02-28 12:08:29 +0900

    ⚡ :: (#72)[Export] Add BASE_URL

--- src/lib/api/index.js
+++
@@ -1,1 +0,0 @@
-export const BASE_URL = "http://3.36.108.34:8080";
---
+++ src/lib/export/index.js
@@ -0,0 +1,4 @@
+export const TOKEN =
+  "http://daplus.net/oauth-oauth-2-0-%EB%B2%A0%EC%96%B4%EB%9F%AC-%ED%86%A0%ED%81%B0%EC%9D%80-%EC%A0%95%ED%99%95%ED%9E%88-%EB%AC%B4%EC%97%87%EC%9E%85%EB%8B%88%EA%B9%8C/";
+
+export const BASE_URL = "http://3.36.108.34:8080";
